0fbf4cec1407c1892dbcd0cf7f3b0f76e74c793e
Gogs <gogs@fake.local>
2018-02-07 11:31:13 +0000

    Refactoring

--- src/main/java/com/aboucham/Application.java
+++ src/main/java/com/aboucham/Application.java
@@ -3,8 +3,6 @@ package com.aboucham;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
-import org.apache.camel.builder.RouteBuilder;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 // load regular Spring XML file from the class path that contains the Camel XML DSL
@@ -15,24 +13,5 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    /*@Component
-    class RestApi extends RouteBuilder {
-
-        @Override
-        public void configure() {
-            /*restConfiguration()
-                    .component("restlet")
-                    //.bindingMode(off)
-                    .port("5117")
-                    .scheme("https")
-                    .endpointProperty("sslContextParameters", "#MySslContextParameter");
-
-            rest("/books").description("Books REST service")
-                    .get("/").description("The list of all the books")
-                    .route().routeId("books-api")
-                    .to("direct:myExample")
-                    .endRest();
-        }
-    }*/
 }
 
